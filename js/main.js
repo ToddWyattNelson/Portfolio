0396d0efb35fbf6178c7e26993c9335feacba140
Wyatt Nelson <wyattnelson95@gmail.com>
2021-05-17 17:57:52 -0600

    added new weeks

--- js/main.js
+++ js/main.js
@@ -1,34 +1,47 @@
-window.onload = setTableofContents => {
-    console.log("wyatt")
-    let list = document.getElementById("ToC");
-    
-    const links = [
-        {
-          label: "Week1 notes",
-          url: "week1/index.html"
-        },
-        {
-          label: "Week2 notes",
-          url: "week1/index.html"
-        }
-      ]
+window.onload = (setTableofContents) => {
+  console.log("wyatt");
+  let list = document.getElementById("ToC");
 
-    links.forEach(element => {
+  const links = [
+    {
+      label: "Week1 notes",
+      url: "/W01/Week1.html",
+    },
+    {
+      label: "Week2 notes",
+      url: "/W02/Week2.html",
+    },
+    {
+      label: "Week3 notes",
+      url: "/W03/Week3.html",
+    },
+    {
+      label: "Week4 notes",
+      url: "/W04/Week4.html",
+    },
+    {
+      label: "Week5 notes",
+      url: "/W05/Week5.html",
+    },
+    {
+      label: "Week6 notes",
+      url: "/W06/Week6.html",
+    },
+  ];
 
-        // Create new Elements
-        let li = document.createElement("li");
-        let a = document.createElement("a");
-        
-        // Add the stuffing
-        a.href = element.url;
-        a.textContent = element.label;
+  links.forEach((element) => {
+    // Create new Elements
+    let li = document.createElement("li");
+    let a = document.createElement("a");
 
-        // append the children
-        li.appendChild(a);
-        list.appendChild(li)
+    // Add the stuffing
+    a.href = element.url;
+    a.textContent = element.label;
 
-        console.log("it worked")
-        
-    });
-      
-}
+    // append the children
+    li.appendChild(a);
+    list.appendChild(li);
+
+    console.log("it worked");
+  });
+};

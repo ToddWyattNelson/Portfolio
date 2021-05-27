4201e6e0fc24e6631d91d4f06ed79a87c5aef3d5
Wyatt Nelson <wyattnelson95@gmail.com>
2021-05-27 10:07:48 -0600

    added slash

--- js/main.js
+++ js/main.js
@@ -5,39 +5,39 @@ window.onload = (setTableofContents) => {
   const links = [
     {
       label: "fun code",
-      url: "Portfolio/fun_code",
+      url: "/Portfolio/fun_code",
     },
     {
       label: "Week1",
-      url: "Portfolio/W01/Week1.html",
+      url: "/Portfolio/W01/Week1.html",
     },
     {
       label: "Week2",
-      url: "Portfolio/W02/Week2.html",
+      url: "/Portfolio/W02/Week2.html",
     },
     {
       label: "Week3",
-      url: "Portfolio/W03/Week3.html",
+      url: "/Portfolio/W03/Week3.html",
     },
     {
       label: "Week4",
-      url: "Portfolio/W04/Week4.html",
+      url: "/Portfolio/W04/Week4.html",
     },
     {
       label: "Week4_b",
-      url: "Portfolio/W04_2/Week4.html",
+      url: "/Portfolio/W04_2/Week4.html",
     },
     {
       label: "Week5",
-      url: "Portfolio/W05/Week5.html",
+      url: "/Portfolio/W05/Week5.html",
     },
     {
       label: "ToDo App1(not working)",
-      url: "Portfolio/Todo_app/todoapp.html",
+      url: "/Portfolio/Todo_app/todoapp.html",
     },
     {
       label: "ToDo App2(Working)",
-      url: "Portfolio/Todo_app_2/todoapp.html",
+      url: "/Portfolio/Todo_app_2/todoapp.html",
     },
   ];
 
